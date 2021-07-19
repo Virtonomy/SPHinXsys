ebd282d97808ff7e681a38a7ead04cf3ffc98463
wen-yang chu <chu@virtonomy.io>
2021-07-19 18:37:53 -0400

    remove TBB:malloc

--- SPHINXsys/src/shared/meshes/my_memory_pool.h
+++ SPHINXsys/src/shared/meshes/my_memory_pool.h
@@ -15,15 +15,14 @@ using namespace tbb;
 template<class T>
 class MyMemoryPool {
 	T sample;
-	tbb::memory_pool< std::allocator<T> > my_pool;				//memory pool
-	typedef tbb::memory_pool_allocator<T> pool_allocator_t;		//memory allocator
-	std::list<T, pool_allocator_t> data_list;					//list of all nodes allocated
+	std::allocator<T> my_pool;				//memory pool
+	std::list<T> data_list;					//list of all nodes allocated
 	std::list<T*> free_list;									//list of all free nodes
 
 public:
 
 	//constructor
-	MyMemoryPool() : data_list((pool_allocator_t(my_pool))) {};
+	MyMemoryPool() : data_list(my_pool) {};
 	//deconstructor
 	~MyMemoryPool() {
 		//my_pool.recycle();

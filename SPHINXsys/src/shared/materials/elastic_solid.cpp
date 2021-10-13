68d076679da0a971cab1d4dbe63116fb3d24fcec
BenceVirtonomy <rochlitz@virtonomy.io>
2021-10-13 14:23:21 +0000

    remove line

--- SPHINXsys/src/shared/materials/elastic_solid.cpp
+++ SPHINXsys/src/shared/materials/elastic_solid.cpp
@@ -129,7 +129,6 @@ namespace SPH {
 	{
 		return  0.5 * K0_ * (J * J - 1);
 	}
-
 	//=================================================================================================//
 	OrthotropicSolid::OrthotropicSolid(Real rho_0, std::array<Vecd, 3> a, std::array<Real, 3> E, std::array<Real, 3> G,std::array<Real, 3> poisson)
 	// we take the max. E and max. possion to approxiamte the maximum of the Bulk modulus --> for time step size calculation

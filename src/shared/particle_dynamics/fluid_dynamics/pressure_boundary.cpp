309dbf2791afa5d8a20c4719edee011529842c89
yu <liao@virtonomy.io>
2024-02-20 10:22:02 +0100

    temporary pressure boundary patch

---
+++ src/shared/particle_dynamics/fluid_dynamics/pressure_boundary.cpp
@@ -0,0 +1,26 @@
+#include "pressure_boundary.h"
+
+namespace SPH
+{
+//=================================================================================================//
+namespace fluid_dynamics
+{
+//=================================================================================================//
+FlowPressureBuffer::FlowPressureBuffer(BodyPartByCell &body_part, Vecd normal_vector)
+    : BaseFlowBoundaryCondition(body_part),
+      kernel_sum_(*particles_->getVariableByName<Vecd>("KernelSummation")), direction_(normal_vector)
+{
+
+    std::cout << "kernel_sum_ size" << kernel_sum_.size() << std::endl;
+};
+//=================================================================================================//
+void FlowPressureBuffer::update(size_t index_i, Real dt)
+{
+    vel_[index_i] += 2.0 * kernel_sum_[index_i] * getTargetPressure(dt) / rho_[index_i] * dt;
+
+    vel_[index_i] = vel_[index_i].dot(direction_) * direction_;
+}
+} // namespace fluid_dynamics
+  //=================================================================================================//
+} // namespace SPH
+  //=================================================================================================//

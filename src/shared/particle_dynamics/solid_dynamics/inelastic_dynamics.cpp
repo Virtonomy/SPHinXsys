9e7049247bdd4aabca0ffc8ab4fc47550255e77c
weiyi <kong@virtonomy.io>
2026-04-15 12:39:42 +0000

    fix plastic

--- src/shared/particle_dynamics/solid_dynamics/inelastic_dynamics.cpp
+++ src/shared/particle_dynamics/solid_dynamics/inelastic_dynamics.cpp
@@ -10,7 +10,6 @@ PlasticIntegration1stHalf::
     PlasticIntegration1stHalf(BaseInnerRelation &inner_relation) : Integration1stHalf(inner_relation),
                                                                    plastic_solid_(DynamicCast<PlasticSolid>(this, elastic_solid_))
 {
-    numerical_dissipation_factor_ = 0.5;
 }
 //=================================================================================================//
 void PlasticIntegration1stHalf::initialization(size_t index_i, Real dt)

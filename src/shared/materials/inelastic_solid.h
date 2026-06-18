7fd5c857c11637db08e74b2a3ae1af5e30fb4c01
Christina-virtonomy <nuss-brill@virtonomy.io>
2026-06-18 13:00:01 +0000

    add multilinear plastic hardening model

--- src/shared/materials/inelastic_solid.h
+++ src/shared/materials/inelastic_solid.h
@@ -20,6 +20,9 @@
  * copy of the License at http://www.apache.org/licenses/LICENSE-2.0.        *
  *                                                                           *
  * ------------------------------------------------------------------------- */
+
+#ifndef VIRTOSIM_INELASTIC_SOLID_H_D1F4B911_43D9_4317_A7AE_48A4E224EEBF
+#define VIRTOSIM_INELASTIC_SOLID_H_D1F4B911_43D9_4317_A7AE_48A4E224EEBF
 /**
  * @file 	inelastic_solid.h
  * @brief 	These are classes for define properties of elastic solid materials.
@@ -28,9 +31,9 @@
  *			and anisotropic muscle, are derived from the basic elastic solid class.
  * @author	Xiangyu Hu and Chi Zhang
  */
-#pragma once
 
 #include "elastic_solid.h"
+#include <unsupported/Eigen/Splines>
 
 namespace SPH
 {
@@ -50,7 +53,7 @@ class PlasticSolid : public NeoHookeanSolid
     {
         material_type_name_ = "PlasticSolid";
     };
-    virtual ~PlasticSolid(){};
+    virtual ~PlasticSolid() {};
 
     Real YieldStress() { return yield_stress_; };
     /** compute the stress through deformation, and plastic relaxation. */
@@ -78,7 +81,7 @@ class HardeningPlasticSolid : public PlasticSolid
     {
         material_type_name_ = "HardeningPlasticSolid";
     };
-    virtual ~HardeningPlasticSolid(){};
+    virtual ~HardeningPlasticSolid() {};
 
     virtual void initializeLocalParameters(BaseParticles *base_particles) override;
     Real HardeningModulus() { return hardening_modulus_; };
@@ -87,4 +90,35 @@ class HardeningPlasticSolid : public PlasticSolid
 
     virtual HardeningPlasticSolid *ThisObjectPtr() override { return this; };
 };
+
+class MultiLinearHardeningPlasticSolid : public PlasticSolid
+{
+  protected:
+    const Real sqrt_2_over_3_ = sqrt(2.0 / 3.0);
+    StdLargeVec<Matd> inverse_plastic_strain_; /**< inverse of plastic right cauchy green strain tensor */
+    StdLargeVec<Real> hardening_parameter_;    /**< hardening parameter */
+    StdLargeVec<Real> hardening_modulus_;      /**< hardening modulus */
+
+    Eigen::Spline<Real, 1, 1> hardening_stresses_;  /**< the yield stress corresponding to different hardening parameters from the spline */
+    Eigen::Spline<Real, 1, 1> hardening_moduluses_; /**< the hardening modulus corresponding to different hardening parameters from the spline */
+
+  public:
+    /** Constructor */
+    explicit MultiLinearHardeningPlasticSolid(Real rho0, Real youngs_modulus, Real poisson_ratio, Real yield_stress,
+                                              const Eigen::Spline<Real, 1, 1> &hardening_moduluses, const Eigen::Spline<Real, 1, 1> &hardening_stresses)
+        : PlasticSolid(rho0, youngs_modulus, poisson_ratio, yield_stress),
+          hardening_moduluses_(hardening_moduluses), hardening_stresses_(hardening_stresses)
+    {
+        material_type_name_ = "MultiLinearHardeningPlasticSolid";
+    };
+    virtual ~MultiLinearHardeningPlasticSolid() {};
+
+    virtual void initializeLocalParameters(BaseParticles *base_particles) override;
+    /** compute the stress through deformation, and plastic relaxation. */
+    virtual Matd PlasticConstitutiveRelation(const Matd &deformation, size_t index_i, Real dt = 0.0) override;
+
+    virtual MultiLinearHardeningPlasticSolid *ThisObjectPtr() override { return this; };
+};
 } // namespace SPH
+
+#endif // VIRTOSIM_INELASTIC_SOLID_H_D1F4B911_43D9_4317_A7AE_48A4E224EEBF
